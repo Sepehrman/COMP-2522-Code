b80af86778f6f937c8db85e6da4f1dd19efb251e
Chris Thompson <cthompson98@bcit.ca>
2020-09-07 20:17:54 -0700

    COMP 2522 Code for 202030. Hello world! Week 1 added.

---
+++ src/ca/bcit/comp2522/code/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package ca.bcit.comp2522.code.helloworld;
+
+/**
+ * Welcome to COMP 2522.
+ *
+ * @author BCIT
+ * @version 202030
+ */
+public class HelloWorld {
+
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(String[] args) {
+        final String message = "Hello world!";
+        System.out.println(message);
+    }
+}
---
+++ src/ca/bcit/comp2522/code/printingAndConcatenation/Addition.java
@@ -0,0 +1,23 @@
+package ca.bcit.comp2522.code.printingAndConcatenation;
+
+/**
+ * Demonstrates the difference between the addition and string concatenation
+ * operators.
+ *
+ * @author Lewis & Loftus 9e
+ * @author BCIT
+ * @version 2020
+ */
+public class Addition {
+
+    /**
+     * Concatenates and adds two numbers, then prints the results.
+     *
+     * @param args command line arguments (unused)
+     */
+    public static void main(String[] args) {
+        System.out.println("24 and 45 concatenated: " + 24 + 45);
+        System.out.println("24 and 45 added: " + (24 + 45));
+    }
+}
+
