a4a849e9761a9630244e74b9721e2634e17b19ea
Chris Thompson <cthompson98@bcit.ca>
2020-10-13 12:13:27 -0700

    Pushed ArrayList code.

--- src/ca/bcit/comp2522/code/generics/OrderedPair.java
+++ src/ca/bcit/comp2522/code/generics/OrderedPair.java
@@ -23,20 +23,6 @@ public class OrderedPair<K, V> {
     }
 
     /**
-     * Returns true if the two specified ordered pairs are equivalent, else
-     * false.
-     *
-     * @param p1
-     * @param p2
-     * @return true if the parameter objects are equivalent
-     */
-    public static <K, V> boolean equivalent(OrderedPair<K, V> p1,
-                                            OrderedPair<K, V> p2) {
-        return p1.getFirst().equals(p2.getFirst()) &&
-                p1.getSecond().equals(p2.getSecond());
-    }
-
-    /**
      * @return first the key
      */
     public K getFirst() {
@@ -48,5 +34,19 @@ public class OrderedPair<K, V> {
      */
     public V getSecond() {
         return second;
+    }
+
+    /**
+     * Returns true if the two specified ordered pairs are equivalent, else
+     * false.
+     *
+     * @param p1
+     * @param p2
+     * @return true if the parameter objects are equivalent
+     */
+    public static <K, V> boolean equivalent(OrderedPair<K, V> p1,
+                                            OrderedPair<K, V> p2) {
+        return p1.getFirst().equals(p2.getFirst()) &&
+                p1.getSecond().equals(p2.getSecond());
     }
 }
